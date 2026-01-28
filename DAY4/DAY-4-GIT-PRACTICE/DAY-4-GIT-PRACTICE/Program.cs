10b13a6e37d759e243430d7e810e0d06718ff892
Kshitij1ghk <kshitijwk@gmail.com>
2026-01-29 02:18:14 +0530

    -implemented 1. Catch invalid input in an exception 2. Throw exceptions intentionally 3. Throw user defined exception

---
+++ DAY4/DAY-4-GIT-PRACTICE/DAY-4-GIT-PRACTICE/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_4_GIT_PRACTICE
+{
+    internal class Program
+    {
+        class invalidmarksException : Exception
+        {
+            public invalidmarksException(string message) : base(message)
+            {
+
+            }
+        }
+        static void checkmarks(int marks)
+        {
+            if(marks<0|| marks > 100)
+            {
+                throw new invalidmarksException("marks must be between 0 and 100");
+            }
+            Console.WriteLine("marks accepted");
+        }
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArgumentException("access denied -you must be at least 18");
+            }
+            else
+            {
+                Console.WriteLine("access granted- you are old enough");
+            }
+        }
+        static void Witdraw(int balance,int amount)
+        {
+            if (amount > balance)
+            {
+                throw new InvalidOperationException("insufficient balance");
+            }
+            Console.WriteLine("withdar succesful");
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+                checkAge(age);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("invalid input. please enter a number");
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("soething went wrong");
+            }
+            //use multiple catch blocks to handle different failure scenarios separately,
+            //giving specific feedback for known exceptions and a generic fallback for unexpected ones.
+            Witdraw(20000, 100000);
+            //task 3
+            try
+            {
+                Console.WriteLine("ener marks:");
+                int marks = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(invalidmarksException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("enter a valid number");
+            }
+        }
+    }
+}
---
+++ DAY4/DAY-4-PRACTICE/DAY-4-PRACTICE/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_4_PRACTICE
+{
+   
+    internal class Program
+    {
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("access denied -you must be at least 18");
+            }
+            else
+            {
+                Console.WriteLine("access granted- you are old enough");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] numbers = { 1, 2, 3 };
+                Console.WriteLine(numbers[10]);
+
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // another way if you want your own message
+            try
+            {
+                int[] mynumbers = { 1, 2, 3 };
+                Console.WriteLine(mynumbers[10]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("something went wrong");
+            }
+            finally
+            {
+                Console.WriteLine("the try catch is finished");
+            }
+            //using throw new exceptions
+            checkAge(15);
+            
+
+        }
+    }
+}
