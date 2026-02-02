e87bc410c1b3fa00795be8d25479831908c05c0d
Kshitij1ghk <kshitijwk@gmail.com>
2026-02-02 18:39:25 +0530

    Wrote a grading system - input score, output grade using if/else and switch

---
+++ DAY6/DAY-6-GIT-PRACTICE/DAY-6-GIT-PRACTICE/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_6_GIT_PRACTICE
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your marks");
+            int marks1 = Convert.ToInt32(Console.ReadLine());
+
+            switch (marks1)
+            {
+                case int m when m < 0 || m > 100:
+                    Console.WriteLine("invalid marks");
+                    break;
+                case int m when m >= 90:
+                    Console.WriteLine("Grade A");
+                    break;
+
+                case int m when m >= 75:
+                    Console.WriteLine("Grade B");
+                    break;
+
+                case int m when m >= 60:
+                    Console.WriteLine("Grade C");
+                    break;
+
+                default:
+                    Console.WriteLine("Fail");
+                    break;
+            }
+
+        }
+    }
+}
